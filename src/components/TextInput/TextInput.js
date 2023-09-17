6029649c7c57ced1aa0dcaf80b05fed083afec1f
Adrian Średniawski <sredni98123@gmail.com>
2023-09-17 19:34:09 +0200

    Add Card and new column

--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -4,6 +4,8 @@ const TextInput = (props) => {
   return (
     <input
       className={styles.input}
+      value={props.value}
+      onChange={props.onChange}
       placeholder={props.placeholder}
       type='text'
     />
