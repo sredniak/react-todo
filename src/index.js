a45f03edd438e109aedfc0c34cd3bdee4f8c2134
Adrian Średniawski <sredni98123@gmail.com>
2023-09-19 08:34:39 +0200

    Fix console error

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from './redux/store';
@@ -8,13 +8,17 @@ import './styles/global.scss';
 import 'font-awesome/css/font-awesome.min.css';
 import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
+);
+
+root.render(
+  
     <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
     </BrowserRouter>
-  </React.StrictMode>,
-  document.querySelector('#root')
-);
+  
+  );
